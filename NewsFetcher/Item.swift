98b5f021906021cf8f12155a1f3b5773d3fc0707
calebwolfe <caleb-wolfe@gmx.com>
2025-10-26 15:48:47 -0600

    Initial remote commit.

---
+++ NewsFetcher/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NewsFetcher
+//
+//  Created by Caleb Wolfe on 10/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NewsFetcherTests/NewsFetcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFetcherTests.swift
+//  NewsFetcherTests
+//
+//  Created by Caleb Wolfe on 10/26/25.
+//
+
+import Testing
+@testable import NewsFetcher
+
+struct NewsFetcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
