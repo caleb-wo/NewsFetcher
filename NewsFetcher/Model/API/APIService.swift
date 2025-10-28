f44433a9cae3e84620aa19ba9356f8d28164158d
calebwolfe <caleb-wolfe@gmx.com>
2025-10-28 00:26:02 -0600

    Fetch api-key from Secrets.plist

--- NewsFetcher/Model/API/APIService.swift
+++ NewsFetcher/Model/API/APIService.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 public struct APIService {
+    private let apiKey: String? = {
+        guard let path = Bundle.main.url(forResource: "Secrets",
+                                        withExtension: "plist"),
+              let data = try? Data(contentsOf: path) else {
+            print("Error: Could not find Secrets.plist or read data.")
+            return nil
+        }
+
+        do {
+            let decoder = PropertyListDecoder()
+            let secrets = try decoder.decode(Secrets.self, from: data)
+            return secrets.NewsApiKey
+        } catch {
+            print("Error decoding plist: \(error)")
+            return nil
+        }
+    }()
     
+}
+
+struct Secrets: Decodable {
+    let NewsApiKey: String
+}
+
+enum APIError: Error {
+    case invalidURL
+    case badServerResponse
+    case decodingError
+    case plistDecodingError
 }
